be29f18259fd600c005e4ab0e967b13a6566a906
vinicio00097 <47514099+vinicio00097@users.noreply.github.com>
2020-11-05 14:23:24 -0600

    last

--- src/main/java/proyectofinal/backend/clinica/ClinicaApplication.java
+++ src/main/java/proyectofinal/backend/clinica/ClinicaApplication.java
@@ -2,12 +2,18 @@ package proyectofinal.backend.clinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ClinicaApplication {
+public class ClinicaApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClinicaApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(ClinicaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ClinicaApplication.class);
     }
 
 }
